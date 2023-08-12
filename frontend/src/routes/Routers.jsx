39681d4d2426865a897cc29ccbb4e9c33014c217
priyanshumaitra <priyanshumaitra@gmail.com>
2023-08-12 15:42:43 +0530

    fixes

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -4,14 +4,14 @@ import Services from '../pages/Services'
 import Login from '../pages/Login'
 import Signup from '../pages/Signup'
 import Contact from '../pages/Contact'
-import Doctors from '../pages/Doctors'
-import DoctorDetails from '../pages/DoctorDetails'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorDetails from '../pages/Doctors/DoctorDetails'
 
 import{Routes, Route} from 'react-router-dom'
 const Routers = () => {
   return <Routes>
     <Route path="/" element={<Home/>} />
-    <Route path="/" element={<Home/>} />
+    <Route path="/home" element={<Home/>} />
     <Route path="/" element={<Home/>} />
     <Route path="/" element={<Home/>} />
     <Route path="/" element={<Home/>} />
