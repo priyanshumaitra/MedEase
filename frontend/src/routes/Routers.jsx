53336c203afe4d38b9fb651f5a81d688d817efef
priyanshumaitra <priyanshumaitra@gmail.com>
2023-08-12 15:23:29 +0530

    project setup

---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,22 @@
+
+import Home from '../pages/Home'
+import Services from '../pages/Services'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Contact from '../pages/Contact'
+import Doctors from '../pages/Doctors'
+import DoctorDetails from '../pages/DoctorDetails'
+
+import{Routes, Route} from 'react-router-dom'
+const Routers = () => {
+  return <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/" element={<Home/>} />
+    <Route path="/" element={<Home/>} />
+    <Route path="/" element={<Home/>} />
+    <Route path="/" element={<Home/>} />
+    <Route path="/" element={<Home/>} />
+    <Route path="/" element={<Home/>} />
+};
+
+export default Routers;
